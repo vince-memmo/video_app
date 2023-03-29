7c1aabcd25c0f63c9c4c8dd984006827f4ea3b67
Vince Memmo <vmemmo1@gmail.com>
2023-03-28 18:01:03 -0700

    users controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -30,32 +30,3 @@ class Api::SessionsController < ApplicationController
   end
 end
 
-def show
-  if current_user
-    @user = current_user
-    # render 'api/users/show'
-    render json: { user: @user } 
-  else
-    render json: { user: nil }
-  end
-end
-
-def create
-  debugger
-  @user = User.find_by_credentials(params[:username], params[:password])
-
-  if @user
-      login!(@user)
-      render json: { user: @user } 
-      # render 'api/users/show'
-      #go here
-    else
-      render json: { errors: ['The provided credentials were invalid.']}, status: 422
-  end
-end
-
-def destroy
-    logout!
-    render json: { message: 'success' }
-end
-end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -24,3 +24,27 @@ class Api::UsersController < ApplicationController
     params.require(:user).permit(:email, :username, :password)
   end
 end
+
+def index
+  @users = User.all
+  render :index
+end
+
+def create
+  @user = User.new(user_params)
+  
+  if @user.save
+    login!(@user)
+    # render json: @user
+    render :show
+  else
+    render json: { errors: @user.errors.full_messages }, status: 422
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:email, :username, :password)
+end
+end
